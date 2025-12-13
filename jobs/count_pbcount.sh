4257fba4dcf1bd8adbfb8a69d8eec2486e359a30
Jan Baudisch <jan.baudisch@uni-ulm.de>
2025-12-13 14:47:42 +0100

    add result collection script

---
+++ jobs/count_pbcount.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+pbcount_logfile=$1
+
+grep -E "^s mc .*\..*$" $pbcount_logfile | cut -d " " -f 3 | cut -d "." -f 1
---
+++ jobs/run_collect.sh
@@ -0,0 +1,4 @@
+input=$1
+csv=$2
+
+sbatch --job-name collect --output collect.out --error collect.err collect.sh $input $csv
