5cde81bc0f1a7fb0e400019e135d1ec08730b1f6
Jan Baudisch <jan.baudisch@uni-ulm.de>
2026-05-20 10:33:59 +0200

    support timeout in local mode

--- jobs/convert_opb.sh
+++ jobs/convert_opb.sh
@@ -4,6 +4,7 @@
 #SBATCH --partition=cpu
 #SBATCH --mem=32gb
 
+TIMEOUT_SECONDS=600
 CONTAINER_IMAGE="${CONTAINER_IMAGE:-ghcr.io/tubs-isf/pb-ddnnf-eval:main-amd64}"
 CONTAINER_MODE="${CONTAINER_MODE:-apptainer}"
 
@@ -18,13 +19,21 @@ case "$CONTAINER_MODE" in
   apptainer exec -B $tmpdir:/out "docker://$CONTAINER_IMAGE" time -f "%e" -o /out/time converter $uvl_file /out/out opb
   ;;
 "podman")
-  podman run -v $tmpdir:/out -v $(realpath $uvl_file):/input $CONTAINER_IMAGE time -f "%e" -o /out/time converter /input /out/out opb
+  podman run -v $tmpdir:/out -v $(realpath $uvl_file):/input $CONTAINER_IMAGE timeout $TIMEOUT_SECONDS time -f "%e" -o /out/time converter /input /out/out opb
   ;;
 *)
   echo "Unknown CONTAINER_MODE!"
   exit 1
   ;;
 esac
+
+if [ "$SLURM_MODE" = "local" ] && [ $? -ne 124 ]; then
+  echo "COMPLETED"
+fi
+
+if [ "$SLURM_MODE" = "local" ] && [ $? -eq 124 ]; then
+  echo "TIMEOUT"
+fi
 
 mv "${tmpdir}/out" $output
 mv "${tmpdir}/time" $timefile
--- jobs/pbcount.sh
+++ jobs/pbcount.sh
@@ -4,6 +4,7 @@
 #SBATCH --partition=cpu
 #SBATCH --mem=32gb
 
+TIMEOUT_SECONDS=600
 CONTAINER_IMAGE="${CONTAINER_IMAGE:-ghcr.io/tubs-isf/pb-ddnnf-eval:main-amd64}"
 CONTAINER_MODE="${CONTAINER_MODE:-apptainer}"
 
@@ -19,12 +20,20 @@ case "$CONTAINER_MODE" in
   apptainer exec -B $tmpdir:/out "docker://${CONTAINER_IMAGE}" time -f "%e" -o /out/time pbcount --cf /out/in
   ;;
 "podman")
-  podman run -v $tmpdir:/out $CONTAINER_IMAGE time -f "%e" -o /out/time pbcount --cf /out/in
+  podman run -v $tmpdir:/out $CONTAINER_IMAGE timeout $TIMEOUT_SECONDS time -f "%e" -o /out/time pbcount --cf /out/in
   ;;
 *)
   echo "Unknown CONTAINER_MODE!"
   exit 1
   ;;
 esac
+
+if [ "$SLURM_MODE" = "local" ] && [ $? -ne 124 ]; then
+  echo "COMPLETED"
+fi
+
+if [ "$SLURM_MODE" = "local" ] && [ $? -eq 124 ]; then
+  echo "TIMEOUT"
+fi
 
 mv "${tmpdir}/time" $timefile
